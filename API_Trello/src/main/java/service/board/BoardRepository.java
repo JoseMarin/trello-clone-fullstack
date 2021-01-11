6077a878f8b5d514af33fe09eebb5001d6f0092b
Jose <jmarinmalagon@gmail.com>
2021-01-11 21:04:56 +0100

    initial commit

---
+++ API_Trello/src/main/java/service/board/BoardRepository.java
@@ -0,0 +1,8 @@
+package service.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface BoardRepository extends JpaRepository<BoardModel, Long> {
+}
---
+++ API_Trello/src/main/java/service/card/CardListResponse.java
@@ -0,0 +1,11 @@
+package service.card;
+
+import java.util.List;
+
+public class CardListResponse {
+    public CardListResponse(List<CardModel> cards) {
+        this.cards = cards;
+    }
+
+    public List<CardModel> cards;
+}
